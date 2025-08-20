e5398e2f6d49702bd39b8dcaed4cef9ed07396ea
vksgaur <132918528+vksgaur@users.noreply.github.com>
2025-08-20 11:52:48 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+// This file contains your secret Firebase configuration.
+// IMPORTANT: Add this filename to your .gitignore file to prevent it from being committed to a public repository!
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBD9Q-GgmHv2K3DaNGXAGMQANH59SGGpog",
+    authDomain: "reader-highlighter.firebaseapp.com",
+    projectId: "reader-highlighter",
+    storageBucket: "reader-highlighter.firebasestorage.app",
+    messagingSenderId: "515869894530",
+    appId: "1:515869894530:web:22f3a4adeb3c7a44e8919b",
+    measurementId: "G-QFW13VXR74"
+};
